4c3f659fae1f1e437eb27ce2af79893340e61512
Oleksii Kolesnyk <oleksii.kolesnyk.w@gmail.com>
2025-02-09 00:43:04 +0200

    Fix rabbit config

--- TestMassTransit.AppHost/Program.cs
+++ TestMassTransit.AppHost/Program.cs
@@ -2,14 +2,17 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var rabbitmq = builder.AddRabbitMQ("rabbitmq")
-    .WithManagementPlugin();
+var rabbitmq = builder
+    .AddRabbitMQ("rabbitmq", password: builder.AddParameter("RabbitmqPassword", secret: true))
+    .WithManagementPlugin(15673);
 
 builder.AddProject<TestMassTransit_Sender>("sender")
     .WithExternalHttpEndpoints()
-    .WithReference(rabbitmq);
+    .WithReference(rabbitmq)
+    .WaitFor(rabbitmq);
 
 builder.AddProject<TestMassTransit_Receiver>("receiver")
-    .WithReference(rabbitmq);;
+    .WithReference(rabbitmq)
+    .WaitFor(rabbitmq);
 
 builder.Build().Run();
