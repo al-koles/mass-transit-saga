254aea34a3d98af52f286fd3bd502ead307ff421
Oleksii Kolesnyk <oleksii.kolesnyk.w@gmail.com>
2025-02-09 22:08:34 +0200

    Add elasticsearch

--- TestMassTransit.AppHost/Program.cs
+++ TestMassTransit.AppHost/Program.cs
@@ -2,17 +2,26 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var elasticsearch = builder.AddElasticsearch("elasticsearch",
+    password: builder.AddParameter("ElasticsearchPassword"))
+    .WithDataVolume("elasticsearch_volume");
+
 var rabbitmq = builder
     .AddRabbitMQ("rabbitmq", password: builder.AddParameter("RabbitmqPassword", secret: true))
-    .WithManagementPlugin(15673);
+    .WithManagementPlugin(15673)
+    .WithDataVolume("rabbitmq_volume");
 
 builder.AddProject<TestMassTransit_Sender>("sender")
     .WithExternalHttpEndpoints()
     .WithReference(rabbitmq)
-    .WaitFor(rabbitmq);
+    .WaitFor(rabbitmq)
+    .WithReference(elasticsearch)
+    .WaitFor(elasticsearch);
 
 builder.AddProject<TestMassTransit_Receiver>("receiver")
     .WithReference(rabbitmq)
-    .WaitFor(rabbitmq);
+    .WaitFor(rabbitmq)
+    .WithReference(elasticsearch)
+    .WaitFor(elasticsearch);
 
 builder.Build().Run();
